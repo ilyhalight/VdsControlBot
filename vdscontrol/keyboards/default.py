8f854bcb05e14b00817c62e96e73b7c4c7cd96b8
Toil <toil.official@yandex.com>
2023-02-15 14:22:36 +0300

    renamed from stats to info

--- vdscontrol/keyboards/default.py
+++ vdscontrol/keyboards/default.py
@@ -2,7 +2,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 default_keyboard = ReplyKeyboardMarkup(resize_keyboard = True)
 
-button1 = KeyboardButton('📊 Статистика')
+button1 = KeyboardButton('📰 Информация')
 button2 = KeyboardButton('📝 Список серверов')
 button3 = KeyboardButton('💻 Добавить сервер')
 button4 = KeyboardButton('🗑 Удалить сервер')
