76a6552e01bbfe2af8642b53e42d7851b61a8cc0
Toil <toil.official@yandex.com>
2023-02-15 14:04:44 +0300

    paramiko -> asyncssh

--- vdscontrol/core/commands.py
+++ vdscontrol/core/commands.py
@@ -1,22 +1,32 @@
+import logging
+from socket import gaierror
 from paramiko import AuthenticationException
-from core.ssh_client import open_connection, check_connection, enter_command, close_connection
+from asyncssh import PermissionDenied
+from core.ssh_client import open_connection, enter_command
 
-def get_info(ip: str, user: str, secret: str, port: int = 22):
+logger = logging.getLogger(__name__)
+
+async def get_info(ip: str, user: str, secret: str, port: int = 22):
     try:
-        open_connection(ip, user, secret, port)
-    except AuthenticationException:
+        neofetch_output = await enter_command((ip, user, secret, port), 'neofetch')
+        df_output = await enter_command((ip, user, secret, port), 'df -h')
+    except PermissionDenied:
         return {
             'status': False,
             'error': '❌ Неверный логин или пароль'
         }
-    if not check_connection():
+    except gaierror:
         return {
             'status': False,
-            'error': '❌ Не удалось подключиться к серверу'
+            'error': '❌ Неверный IP адрес'
         }
-    neofetch_output = enter_command('neofetch')
-    df_output = enter_command('df -h')
-    close_connection()
+    except Exception as err:
+        logger.exception(err)
+        return {
+            'status': False,
+            'error': '❌ Не удалось получить информацию о сервере'
+        }
+
     os = neofetch_output.partition('OS')[2].split('[0m')[3]
     host = neofetch_output.partition('Host')[2].split('[0m')[3]
     kernel = neofetch_output.partition('Kernel')[2].split('[0m')[3]
